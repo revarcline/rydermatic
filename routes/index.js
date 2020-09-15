81b70652a4d541c819a5c795e6f176552841e1ff
Alex Cline <rev.a.r.cline@gmail.com>
2020-09-15 10:36:59 -0500

    first failed attempt

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+// load names.json file
+let name_data = require('../names.json');
+
+// parse names.json file for name primitives, generate three-part name
+function makeName(name_list) {
+  var first = name_list.first;
+  var lastone = name_list.lastone;
+  var lasttwo = name_list.lasttwo;
+
+  var rand_first = Math.floor(Math.random()*first.length)
+  var rand_lastone = Math.floor(Math.random()*lastone.length)
+  var rand_lasttwo = Math.floor(Math.random()*lasttwo.length)
+
+  return `${first[rand_first]} ${lastone[rand_lastone]}${lasttwo[rand_lasttwo]}`
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'rydermatic' });
+  var name = makeName(name_data)
+  res.render('index', { title: name });
   next();
 });
 
