7d83f3fa494f2eeee52123f6a13d72a6d97323d1
Alex Cline <rev.a.r.cline@gmail.com>
2020-09-15 08:58:57 -0500

    hello server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).send("yeah baby we're workin")
+})
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+})
