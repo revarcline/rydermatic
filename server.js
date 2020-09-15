56b540688f49691da9935b15222344157dfbf10e
Alex Cline <rev.a.r.cline@gmail.com>
2020-09-15 09:20:30 -0500

    hello pug:

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.use('/', (req, res, next) => {
-    console.log(`URL: ${req.url}`);
-    next();
-});
+const port = 3000;
+const host = 'localhost';
+
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
-app.get('*', (req, res, next) => {
-    res.status(200).send("Sorry, can't find that.");
-    next();
-});
-
-app.listen(port, () => {
+app.listen(port, host, () => {
     console.log(`Server started at port ${port}`);
 });
