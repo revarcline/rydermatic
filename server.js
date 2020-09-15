44446a3eeb3d13311258e421e179053613312583
Alex Cline <rev.a.r.cline@gmail.com>
2020-09-15 09:15:18 -0500

    basic routing

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use('/', (req, res, next) => {
+    console.log(`URL: ${req.url}`);
+    next();
+});
+
 app.get('/', (req, res) => {
     res.status(200).sendFile(__dirname + '/index.html');
-})
+});
+
+app.get('*', (req, res, next) => {
+    res.status(200).send("Sorry, can't find that.");
+    next();
+});
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
-})
+});
