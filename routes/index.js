086759b5056cd4b9088e62318de8e5f53fcb057f
Alex Cline <rev.a.r.cline@gmail.com>
2020-09-15 23:57:01 -0500

    added new logic and divs

--- routes/index.js
+++ routes/index.js
@@ -6,27 +6,31 @@ let name_data = require('../names.json');
 
 // parse names.json file for name primitives, generate three-part name
 function makeName(name_list) {
-  var first = name_list.first;
-  var lastone = name_list.lastone;
-  var lasttwo = name_list.lasttwo;
+  if (Math.floor(Math.random() * 100) == 1) {
+    return "Bob Johnson!";
+  } else { 
+    var first = name_list.first;
+    var lastone = name_list.lastone;
+    var lasttwo = name_list.lasttwo;
 
-  var rfi = Math.floor(Math.random()*first.length)
-  var rlo = Math.floor(Math.random()*lastone.length)
-  var rlt = Math.floor(Math.random()*lasttwo.length)
+    var rfi = Math.floor(Math.random()*first.length);
+    var rlo = Math.floor(Math.random()*lastone.length);
+    var rlt = Math.floor(Math.random()*lasttwo.length);
 
 
-  if (Math.floor(Math.random() * 20) == 1) {
-    return `${first[rfi]} Mc${lastone[rlo]}${lasttwo[rlt]}!`
-  } else {
-    return `${first[rfi]} ${lastone[rlo]}${lasttwo[rlt]}!`
+    if (Math.floor(Math.random() * 20) == 1) {
+      return `${first[rfi]} Mc${lastone[rlo]}${lasttwo[rlt]}!`;
+    } else {
+      return `${first[rfi]} ${lastone[rlo]}${lasttwo[rlt]}!`;
+    }
   }
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(name_data)
-  var name = makeName(name_data)
-  res.render('index', { title: name });
+  console.log(name_data);
+  var name = makeName(name_data);
+  res.render('index', { title: "Bob Johnson!" });
   next();
 });
 
